73c3cd29aee76e3deccd25850c976402ffabbf9b
21up <1109287523@qq.com>
2020-07-14 21:49:51 +0800

    修改SpringCloud Config-Client模块，测试刷新配置,bug：总是为刷新，原因是未按照说明来，先修改仓库中的文件，再执行refresh请求，再发送client的请求

--- config-client/src/main/java/com/wen/cloud/controller/ConfigClientController.java
+++ config-client/src/main/java/com/wen/cloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.wen.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by 21up on 2020/7/14
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
     @Value("${config.info}")
     private String configInfo;
