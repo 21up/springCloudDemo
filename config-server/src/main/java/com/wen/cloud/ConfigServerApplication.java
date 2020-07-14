5db221b4d16d918213b7ec676a4a492250cdb158
21up <1109287523@qq.com>
2020-07-14 17:54:50 +0800

    添加SpringCloud Config模块，测试获取配置信息

---
+++ config-server/src/main/java/com/wen/cloud/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.wen.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by 21up on 2020/7/14
+ */
+@EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
