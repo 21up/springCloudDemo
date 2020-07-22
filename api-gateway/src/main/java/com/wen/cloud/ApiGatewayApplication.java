ca3b20e3e711f11f6e7d0870290b8748270d5916
21up <1109287523@qq.com>
2020-07-22 13:17:24 +0800

    结合注册中心使用

--- api-gateway/src/main/java/com/wen/cloud/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/wen/cloud/ApiGatewayApplication.java
@@ -2,6 +2,7 @@ package com.wen.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import javax.swing.*;
 
@@ -9,6 +10,7 @@ import javax.swing.*;
  * Created by 21up on 2020/7/22
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class);
