bf381067e4157b764b88e6c92131dc2405884b38
21up <1109287523@qq.com>
2020-07-26 15:59:19 +0800

    添加admin-server、admin-client模块

--- admin-client/src/main/java/com/wen/cloud/AdminClientApplication.java
+++ admin-client/src/main/java/com/wen/cloud/AdminClientApplication.java
@@ -2,11 +2,13 @@ package com.wen.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by 21up on 2020/7/22
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdminClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminClientApplication.class);
--- admin-server/src/main/java/com/wen/cloud/AdminServerApplication.java
+++ admin-server/src/main/java/com/wen/cloud/AdminServerApplication.java
@@ -3,12 +3,14 @@ package com.wen.cloud;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by 21up on 2020/7/22
  */
 @EnableAdminServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdminServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminServerApplication.class);
