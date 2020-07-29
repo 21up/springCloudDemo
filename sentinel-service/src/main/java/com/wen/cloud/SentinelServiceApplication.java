a70a2bfc49f98a8d061939eced82b5130e1e20bf
21up <1109287523@qq.com>
2020-07-29 10:55:18 +0800

    新增sentinel-service模块，测试根据资源和url的限流功能

---
+++ sentinel-service/src/main/java/com/wen/cloud/SentinelServiceApplication.java
@@ -0,0 +1,11 @@
+package com.wen.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelServiceApplication.class);
+    }
+}
