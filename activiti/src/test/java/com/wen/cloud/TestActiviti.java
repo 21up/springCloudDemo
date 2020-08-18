9a363528626b2ae1c63170d824083a636cd05b6c
21up <1109287523@qq.com>
2020-08-18 11:17:34 +0800

    Merge branch 'master' of D:\GitProjects\springCloudDemo with conflicts.

---
+++ activiti/src/test/java/com/wen/cloud/TestActiviti.java
@@ -0,0 +1,15 @@
+package com.wen.cloud;
+
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by 21up on 2020/7/21
+ */
+
+public class TestActiviti {
+    @Autowired
+    private ProcessEngineConfiguration processEngineConfiguration;
+
+}
