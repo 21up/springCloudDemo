98dfc8e4894c65363215d8ae24d919d1189369ea
21up <1109287523@qq.com>
2020-09-02 19:58:49 +0800

    activiti生成表的简单方式

--- activiti/src/test/java/com/wen/cloud/TestActiviti.java
+++ activiti/src/test/java/com/wen/cloud/TestActiviti.java
@@ -2,6 +2,7 @@ package com.wen.cloud;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.ProcessEngines;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,6 +17,12 @@ public class TestActiviti {
     public void testGenTable(){
         ProcessEngineConfiguration configuration = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("activiti.cfg.xml");
         ProcessEngine processEngine = configuration.buildProcessEngine();
+        System.out.println(processEngine);
+    }
+    //简单的生成表的方法
+    @Test
+    public void testSimpleGenTable(){
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         System.out.println(processEngine);
     }
 }
